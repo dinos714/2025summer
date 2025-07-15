16d4993b14c36f68ab0b21975cc6595f63b6824a
zhanghp23@mails.tsinghua.edu.cn <zhanghp23@mails.tsinghua.edu.cn>
2025-07-16 00:23:48 +0800

    hw2 hw3 hw4 implemented

--- render.py
+++ render.py
@@ -5,7 +5,6 @@ path = '/sharefs/zhanghp/zhp-private/summer-course-2025/deepul/homeworks/hw3/hw3
 with open(path, "r", encoding="utf-8") as f:
     nb = nbformat.read(f, as_version=4)
 
-# 安全修复 metadata.widgets
 if "widgets" in nb.metadata and "state" not in nb.metadata["widgets"]:
     nb.metadata["widgets"]["state"] = {}
 
