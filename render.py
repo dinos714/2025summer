dd73bb10933ae91fc84fd4e66e98048b4aa7e7ad
zhanghp23@mails.tsinghua.edu.cn <zhanghp23@mails.tsinghua.edu.cn>
2025-07-16 00:31:46 +0800

    fix rendering hw3

--- render.py
+++ render.py
@@ -5,10 +5,13 @@ path = '/sharefs/zhanghp/zhp-private/summer-course-2025/hw3/hw2_notebook.ipynb'
 with open(path, "r", encoding="utf-8") as f:
     nb = nbformat.read(f, as_version=4)
 
-if "widgets" in nb.metadata and "state" not in nb.metadata["widgets"]:
-    nb.metadata["widgets"]["state"] = {}
+if "widgets" not in nb.metadata:
+    nb.metadata["widgets"] = {}
+
+nb.metadata["widgets"].setdefault("state", {})
+nb.metadata["widgets"].setdefault("version", "1.0.0")
 
 with open(path, "w", encoding="utf-8") as f:
     nbformat.write(nb, f)
 
-print(f"{path} finish fixing rendering problem.")
+print(f"{path} finishing fixing rendering problem.")
