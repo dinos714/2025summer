0128d5a175be8c51c74fc2052a89a64ab713c5b4
zhanghp23@mails.tsinghua.edu.cn <zhanghp23@mails.tsinghua.edu.cn>
2025-07-16 00:35:22 +0800

    fix rendering hw3

--- render.py
+++ render.py
@@ -1,17 +1,21 @@
 import nbformat
 
-path = '/sharefs/zhanghp/zhp-private/summer-course-2025/hw3/hw2_notebook.ipynb'
+path = "/sharefs/zhanghp/zhp-private/summer-course-2025/hw3/hw2_notebook.ipynb"
+nb = nbformat.read(open(path, 'r', encoding='utf-8'), as_version=4)
 
-with open(path, "r", encoding="utf-8") as f:
-    nb = nbformat.read(f, as_version=4)
+# 删除 metadata 中的 widgets 字段
+if "widgets" in nb.metadata:
+    del nb.metadata["widgets"]
 
-if "widgets" not in nb.metadata:
-    nb.metadata["widgets"] = {}
-
-nb.metadata["widgets"].setdefault("state", {})
-nb.metadata["widgets"].setdefault("version", "1.0.0")
+# 删除每个 cell 输出中与 widgets 相关的内容
+for cell in nb.cells:
+    if "outputs" in cell:
+        cell["outputs"] = [
+            o for o in cell["outputs"] if o.get("output_type") != "display_data" or
+            "application/vnd.jupyter.widget-view+json" not in o.get("data", {})
+        ]
 
 with open(path, "w", encoding="utf-8") as f:
     nbformat.write(nb, f)
 
-print(f"{path} finishing fixing rendering problem.")
+print("✅ 已删除 widgets 元数据和 cell 中的 widget 输出")
