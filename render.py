814c7edcf979ebe09f195c52b3d032056b42158a
zhanghp23@mails.tsinghua.edu.cn <zhanghp23@mails.tsinghua.edu.cn>
2025-07-16 00:28:16 +0800

    fix rendering hw3

--- render.py
+++ render.py
@@ -1,6 +1,6 @@
 import nbformat
 
-path = '/sharefs/zhanghp/zhp-private/summer-course-2025/deepul/homeworks/hw3/hw3.ipynb'
+path = '/sharefs/zhanghp/zhp-private/summer-course-2025/hw3/hw2_notebook.ipynb'
 
 with open(path, "r", encoding="utf-8") as f:
     nb = nbformat.read(f, as_version=4)
